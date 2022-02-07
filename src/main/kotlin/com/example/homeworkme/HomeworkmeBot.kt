3ed2452f2f7411475a5aed11c523cccd22477fdb
petr <bvasa8558@gmail.com>
2022-02-07 23:25:51 +0300

    add button

--- src/main/kotlin/com/example/homeworkme/HomeworkmeBot.kt
+++ src/main/kotlin/com/example/homeworkme/HomeworkmeBot.kt
@@ -1,14 +1,18 @@
-package com.example.homeworkme.component
+package com.example.homeworkme
 
 import com.example.homeworkme.properties.BotProperties
 import com.example.homeworkme.service.ReceiverService
+import org.slf4j.LoggerFactory
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
 import javax.annotation.PostConstruct
+
 
 @Component
 @EnableConfigurationProperties(BotProperties::class)
@@ -17,6 +21,7 @@ class HomeworkmeBot(
     private val botCommands: List<IBotCommand>,
     private val receiverService: ReceiverService
 ) : TelegramLongPollingCommandBot() {
+    private val log = LoggerFactory.getLogger(HomeworkmeBot::class.java)
 
     @PostConstruct
     fun init() {
@@ -40,7 +45,21 @@ class HomeworkmeBot(
 
     override fun processNonCommandUpdate(update: Update) {
         receiverService.receive(update)
+        val chatId = update.message?.chatId ?: update.callbackQuery?.message?.chatId
+        execute(buildInlineKeyboardSendMessage(chatId ?: log.error("chatId is null").run { return }))
     }
 
-
+    fun buildInlineKeyboardSendMessage(chatId: Long): SendMessage = SendMessage().apply {
+        this.chatId = chatId.toString()
+        text = "Привед 5А (тут будет ДЗ...)"
+        replyMarkup = InlineKeyboardMarkup().apply {
+            keyboard = listOf(
+                listOf(
+                    InlineKeyboardButton().apply {
+                        text = "Дай домашку"
+                        callbackData = "button has been pressed"
+                    })
+            )
+        }
+    }
 }
